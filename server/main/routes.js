27b1019285a1eb7f91cfa896760ca6cc490987d3
FranklinMiranda <fdm61344@gmail.com>
2023-03-09 11:34:35 -0500

    CM

--- server/main/routes.js
+++ server/main/routes.js
@@ -3,7 +3,10 @@ var router = express.Router();
 var pool = require('./db');
 
 router.post('/api/userprofiletodb', (req, res, next) => {
-  const values = [req.body.user.name, req.body.user.email];
+
+
+
+  const values = [req.body.name, req.body.email];
 
   pool.query(
     'INSERT INTO users(username, email, date_created) VALUES($1, $2, NOW()) ON CONFLICT DO NOTHING',
