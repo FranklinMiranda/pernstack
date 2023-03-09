d9fc915fe74f9beb10c545b67c81625e06d5174e
FranklinMiranda <fdm61344@gmail.com>
2023-03-09 10:01:00 -0500

    CM

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
+import LoginButton from "./components/login";
+import LogoutButton from "./components/logout";
+
+
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <LoginButton/>
+      <LogoutButton/>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -5,7 +6,12 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-yfnh5rsri6uihrxp.us.auth0.com"
+      clientId="ppL337UqU34iwSTYaLwObXpXm9hRwdFs"
+      authorizationParams={{ redirect_uri: 'http://localhost:3000' }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
-
