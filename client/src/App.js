041ae4bf750220f59d557020301135ae8ac7cea4
FranklinMiranda <fdm61344@gmail.com>
2023-03-09 10:05:49 -0500

    CM

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import LoginButton from "./components/login";
-import LogoutButton from "./components/logout";
-
+import LoginButton from './components/login';
+import LogoutButton from './components/logout';
+import Profile from './components/profile';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
-      <LoginButton/>
-      <LogoutButton/>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
     </div>
   );
 }
